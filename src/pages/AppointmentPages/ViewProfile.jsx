597767c792b6e331d268c0c653fcbf0915b6cfd8
kristinjuwel <145471931+kristinjuwel@users.noreply.github.com>
2024-01-05 23:11:08 +0800

    Update ViewProfile.jsx

--- src/pages/AppointmentPages/ViewProfile.jsx
+++ src/pages/AppointmentPages/ViewProfile.jsx
@@ -36,7 +36,7 @@ const ViewProfile = () => {
     useEffect(() => {
     const fetchUser = async () => {
         try {
-          const response = await fetch(`http://railway-backend-production-a8c8.up.railway.app/patientview/${patientUserId}`);
+          const response = await fetch(`https://railway-backend-production-a8c8.up.railway.app/patientview/${patientUserId}`);
           if (response.ok) {
             const data = await response.json();
             setUser(data);
@@ -87,7 +87,7 @@ const ViewProfile = () => {
     useEffect( () => {
       const fetchUser = async () => {
         try {
-          const response = await fetch(`http://railway-backend-production-a8c8.up.railway.app/doctordetails/${username}`);
+          const response = await fetch(`https://railway-backend-production-a8c8.up.railway.app/doctordetails/${username}`);
           if (response.ok) {
            setIsDoctorLoggedIn(true);
    
